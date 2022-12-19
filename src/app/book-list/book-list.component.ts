683c011ad0bdebc099a7f1b8596c46680f204705
sstnkvc <sasa.stankovic1996@hotmail.com>
2022-12-19 09:18:35 +0100

    add prototype

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+})
+export class BookListComponent implements OnInit {
+  constructor() {
+    //
+  }
+
+  booksArray: Book[] = [];
+
+  ngOnInit() {
+    this.booksArray = [
+      {
+        isbn: '9783864907791',
+        title: 'Angular',
+        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny'],
+        published: new Date(2020, 8, 1),
+        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practises',
+        rating: 5,
+        thumbnails: [
+          {
+            url: 'https://cdn.ng-buch.de/cover/angular3.jpg',
+            title: 'Buchcover',
+          },
+        ],
+        description: 'Lernen Sie Angular mit diesem Praxisbuch!',
+      },
+      {
+        isbn: '783864905520',
+        title: 'React',
+        authors: ['Oliver Zeigermann', 'Nils Hartmann'],
+        published: new Date(2019, 2, 3),
+        subtitle: 'Grundlagen, fortgeschrittene Techniken und Praxistipps',
+        rating: 3,
+        thumbnails: [
+          {
+            url: 'https://cdn.ng-buch.de/cover/react2.jpg',
+            title: 'Buchcover',
+          },
+        ],
+        description: 'Das bewährte und umfassende Praxisbuch zu React',
+      },
+    ];
+  }
+}
